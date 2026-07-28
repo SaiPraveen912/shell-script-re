ea3ce8f23d2ce9380fa9562b14ba763b64dcb652
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-28 11:27:43 +0530

    variables push from laptop

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-this is agin a sample script
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh:: I am fine too. how is your work?"
+echo "Suresh:: Not bad. I am thinking to upgrade to DevOps"
