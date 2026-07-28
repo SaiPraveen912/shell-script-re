ba738cd6beca8934e7c3be1716451095a1d6b972
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-28 12:14:41 +0530

    special-variables push from laptop

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,3 +3,4 @@
 echo "All variables: $@" # @ is notation of everything
 echo "Number of variables passed: $#" # # is the notation of number of values passed
 echo "To know this Script Name: $0"
+echo "To know current working directory: $PWD"
