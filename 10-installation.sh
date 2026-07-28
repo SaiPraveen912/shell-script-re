be6ac34be4971320cf8680606e4b43810914480e
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-28 13:02:53 +0530

    installation push from laptop

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access"
+    exit 1 # Manually exit if error comes We can use 1 to 127 --> 0 is for success exit status
 else
     echo "You are super user"
 fi
