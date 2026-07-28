577ac4249a645629151e8e2ff577780b60c3e04c
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-28 11:51:15 +0530

    variables and array push from laptop

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTillu" "Murari") #list of movies
+
+# list always start with 0
+# Size of above array is 3
+# index are 0,1,2
+# index is always -1 of the size of the array
+
+echo "First Movie is: ${MOVIES[0]}"
