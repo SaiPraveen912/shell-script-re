d2c4a1d54fcfc912ef1fe48a7e5415f9d2d5668b
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-30 11:27:49 +0530

    functions push from laptop

--- 11-functions-re.sh
+++ 11-functions-re.sh
@@ -2,6 +2,14 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+    else
+        echo "$2...SUCCESS"
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run the script using super user credentials"
@@ -9,5 +17,11 @@ then
 else 
     echo "You are super user"
 fi    
+
+dnf install mysql -y
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y
+VALIDATE $? "Installing Git"
 
 echo "Is script proceeding?"
