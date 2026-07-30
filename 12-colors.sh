68fbb5f0aacaf6f6c5fbbb343633a882a80d10ec
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-30 13:23:49 +0530

    colors functions logfile push from laptop

---
+++ 12-colors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "Script started executing at: $TIMESTAMP"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else 
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1 # Manually exit if error comes We can use 1 to 127 --> 0 is for success exit status
+else
+    echo "You are super user"
+fi
+
+dnf install mysql -y &>>$LOGFILE   # -y is mandatory otherwise shell script will keep on waiting
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
+
