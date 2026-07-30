0cde841fcb729076b8d58470234f1885f711a320
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-30 12:11:57 +0530

    functions logfile push from laptop

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -20,10 +23,10 @@ else
     echo "You are super user"
 fi
 
-dnf install mysql -y    # -y is mandatory otherwise shell script will keep on waiting
+dnf install mysql -y &>>$LOGFILE   # -y is mandatory otherwise shell script will keep on waiting
 VALIDATE $? "Installing MySQL"
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
 
 echo "Is script proceeding?"
