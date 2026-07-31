256ecb546096c503d0e8131e4d4c4b6e60608144
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-31 06:27:01 +0530

    Installing packages as parameters from command line while execution push from laptop

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "Insatlling $2...FAILURE"
+        exit 1
+    else
+        echo "Installing $2...SUCCESS"
+    fi
+}
+
 
 if [ $USERID -ne 0 ]
 then
@@ -14,4 +28,11 @@ fi
 for i in $@
 do
     echo "Package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo "$i already installed...SKIPPING"
+    else
+        echo "$i not installed...Need to install"
+    fi
 done
