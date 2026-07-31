3f7fdde3b910ef7e6c04f8d24de80808f1d82d34
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-07-31 06:43:32 +0530

    Installing packages as parameters from command line while execution push from laptop

---
+++ 15-install-packages-practice.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script using root user credentials"
+    exit 1
+else
+    echo "You are root user"
+fi
+
+echo "Is script proceeding?"
