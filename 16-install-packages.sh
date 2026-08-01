3e449924647a33f3b0b399ac4f071e584ee44dde
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-01 12:05:31 +0530

    Installing packages practice as parameters from command line while execution push from laptop

--- 16-install-packages.sh
+++ 16-install-packages.sh
@@ -5,9 +5,9 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
-G="e\[32m"
-Y="e\[33m"
-N="e\[30m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[30m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
