97ff8f9a54c1d63787a81c40be6932e8ecb8e991
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-01 11:25:35 +0530

    Installing packages practice as parameters from command line while execution push from laptop

--- 16-install-packages.sh
+++ 16-install-packages.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+echo $SCRIPT_NAME
+echo $LOGFILE
+echo $TIMESTAMP
 
 if [ $USERID -ne 0 ]
 then
