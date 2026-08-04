3214e18e539cf5ed1a36b7d752112e49f23c51ac
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-04 12:04:50 +0530

    calling script from another script using shell scripting

--- 17-current-script.sh
+++ 17-current-script.sh
@@ -5,7 +5,7 @@ COURSE="DevOps from current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-#./18-other-script.sh
+# We are calling another script
+./18-other-script.sh
 
 echo "After calling other script, course: $COURSE"
-echo "Process ID of current shell script: $$"
