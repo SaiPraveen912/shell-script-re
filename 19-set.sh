c54ea5068f8f2b5fadd880b8870b9195e435703e
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-05 11:48:06 +0530

    using set -e to check automatic exit if error comes

---
+++ 19-set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1 # Manually exit if error comes We can use 1 to 127 --> 0 is for success exit status
+else
+    echo "You are super user"
+fi
+
+dnf install mysqsdcdsl -y    # -y is mandatory otherwise shell script will keep on waiting
+dnf install git -y
+
+
+
+echo "Is script proceeding?"
