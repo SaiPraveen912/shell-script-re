0831a9f5e68ba4a674f7fa66a1ef4af0d2d68777
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-05 14:11:20 +0530

    Delete old logs

--- 20-delete-old-logs.sh
+++ 20-delete-old-logs.sh
@@ -18,3 +18,9 @@ fi
 FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
 echo "Files to delete: $FILES"
+
+# We are giving above FIles output as Input to while loop
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
+done <<< $FILES # input for this while loop is coming from FILES
