421a4d88dec63d60899d8f4d601c67201c577593
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-08-12 13:59:02 +0530

    ram usage check and sending alert to email

---
+++ 23-memory-usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MEMORY_USAGE=$(free | grep Mem | awk '{print ($3/$2)*100}')
+MEMORY_THRESHOLD=80
+
+echo "Current Memory Usage: $MEMORY_USAGE%"
+
+if [ ${MEMORY_USAGE%.*} -ge $MEMORY_THRESHOLD ]
+then
+    MESSAGE="High Memory Usage on server: Current usage is ${MEMORY_USAGE}%"
+
+    echo "$MESSAGE"
+
+    printf '%b' "$MESSAGE" | mail -s "Memory Usage Alert" saipraveen.immanni@gmail.com
+fi
